0867edfce287fd70c9923920dd8b4fb69fade091
james <james@time4tea.net>
2016-10-10 18:41:19 +0100

    result of the lesson on move instance method

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-    private final String name;
+    private final Tariff tariff = new Tariff();
+    private final LoyaltyPlan loyaltyPlan = new LoyaltyPlan();
     private final List<Rental> rentals = new ArrayList<>();
+    private final String name;
 
     public Customer(String name) {
         this.name = name;
@@ -13,19 +15,15 @@ public class Customer {
         rentals.add(rental);
     }
 
-    private String getName() {
-        return name;
-    }
-
     public String generateStatement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        String result = "Rental Record for " + getName() + "\n";
+        String result = "Rental Record for " + name + "\n";
 
         for (Rental rental : rentals) {
-            double rentalPrice = calculatePrice(rental);
+            double rentalPrice = tariff.calculatePrice(rental);
 
-            frequentRenterPoints += calculatePoints(rental);
+            frequentRenterPoints += loyaltyPlan.calculatePoints(rental);
 
             result += "\t" + rental.getMovie().getTitle() + "\t"
                     + String.valueOf(rentalPrice) + "\n";
@@ -36,39 +34,6 @@ public class Customer {
         result += "You owed " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
 
-
         return result;
-    }
-
-    private int calculatePoints(Rental rental) {
-        int frequentRenterPoints = 0;
-        frequentRenterPoints++;
-
-        if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
-                && rental.getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
-    }
-
-    private double calculatePrice(Rental rental) {
-        double rentalPrice = 0;
-
-        // determines the amount for each line
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                rentalPrice += 2;
-                if (rental.getDaysRented() > 2)
-                    rentalPrice += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                rentalPrice += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                rentalPrice += 1.5;
-                if (rental.getDaysRented() > 3)
-                    rentalPrice += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return rentalPrice;
     }
 }
---
+++ src/main/java/LoyaltyPlan.java
@@ -0,0 +1,11 @@
+public class LoyaltyPlan {
+    public int calculatePoints(Rental rental) {
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+
+        if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
+                && rental.getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+}
