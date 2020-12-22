4291a0760beef2d4ff680266ecbf089bd5e360d9
Juan <earth001@gmail.com>
2020-12-22 18:16:16 -0300

    Added refactor version

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,68 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.Vector;
-import java.util.Enumeration;
+public class Customer {
 
-public class Customer 
-{
-	public Customer (String name) {
-		this.name = name;
-	}
-	
-	public void addRental (Rental rental) {
-		rentals.addElement (rental);
-	}
-	
-	public String getName () {
-		return name;
-	}
-	
-	public String statement () {
-		double 				totalAmount 			= 0;
-		int					frequentRenterPoints 	= 0;
-		Enumeration 		rentals 				= this.rentals.elements ();
-		String 				result 					= "Rental Record for " + getName () + "\n";
-		
-		while (rentals.hasMoreElements ()) {
-			double 		thisAmount = 0;
-			Rental 		each = (Rental)rentals.nextElement ();
-			
-			// determines the amount for each line
-			switch (each.getMovie ().getPriceCode ()) {
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (each.getDaysRented () > 2)
-						thisAmount += (each.getDaysRented () - 2) * 1.5;
-					break;
-				case Movie.NEW_RELEASE:
-					thisAmount += each.getDaysRented () * 3;
-					break;
-				case Movie.CHILDRENS:
-					thisAmount += 1.5;
-					if (each.getDaysRented () > 3)
-						thisAmount += (each.getDaysRented () - 3) * 1.5;
-					break;
-			}
-			
-			frequentRenterPoints++;
-			
-			if (each.getMovie ().getPriceCode () == Movie.NEW_RELEASE 
-					&& each.getDaysRented () > 1)
-				frequentRenterPoints++;
-				
-			result += "\t" + each.getMovie ().getTitle () + "\t"
-								+ String.valueOf (thisAmount) + "\n";
-			totalAmount += thisAmount;
-				
-		}
-		
-		result += "You owed " + String.valueOf (totalAmount) + "\n";
-		result += "You earned " + String.valueOf (frequentRenterPoints) + " frequent renter points\n";
-		
-		
-		return result;
-	}
-	
+    private final String name;
+    private final List<Rental> rentals = new ArrayList<>();
 
-	private String name;
-	private Vector rentals = new Vector ();
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        StringBuilder result = new StringBuilder();
+        result.append("Rental Record for ").append(getName()).append("\n");
+
+        for (Rental each : rentals) {
+            double thisAmount = 0;
+
+            // determines the amount for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            frequentRenterPoints++;
+
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE
+                && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            result.append("\t").append(each.getMovie().getTitle()).append("\t").append(thisAmount)
+                .append("\n");
+            totalAmount += thisAmount;
+
+        }
+
+        result.append("You owed ").append(totalAmount).append("\n");
+        result.append("You earned ").append(frequentRenterPoints)
+            .append(" frequent renter points\n");
+
+        return result.toString();
+    }
 }
--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -7,29 +7,49 @@ public class VideoStoreTest {
 
     @Test
     public void testSingleNewReleaseStatement() {
-        customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
-        Assert.assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
+        customer.addRental(new Rental(newRelease("The Cell"), 3));
+        Assert.assertEquals(
+            "Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n",
+            customer.statement());
+    }
+
+    private Movie newRelease(String title) {
+        return new Movie(title, Movie.NEW_RELEASE);
     }
 
     @Test
     public void testDualNewReleaseStatement() {
-        customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
-        customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
-        Assert.assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement());
+        customer.addRental(new Rental(newRelease("The Cell"), 3));
+        customer.addRental(new Rental(newRelease("The Tigger Movie"), 3));
+        Assert.assertEquals(
+            "Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n",
+            customer.statement());
     }
 
     @Test
     public void testSingleChildrensStatement() {
-        customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
-        Assert.assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
+        customer.addRental(new Rental(newChildrens("The Tigger Movie"), 3));
+        Assert.assertEquals(
+            "Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n",
+            customer.statement());
+    }
+
+    private Movie newChildrens(String title) {
+        return new Movie(title, Movie.CHILDRENS);
     }
 
     @Test
     public void testMultipleRegularStatement() {
-        customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
-        customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
-        customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+        customer.addRental(new Rental(newRegular("Plan 9 from Outer Space"), 1));
+        customer.addRental(new Rental(newRegular("8 1/2"), 2));
+        customer.addRental(new Rental(newRegular("Eraserhead"), 3));
 
-        Assert.assertEquals("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.statement());
+        Assert.assertEquals(
+            "Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n",
+            customer.statement());
+    }
+
+    private Movie newRegular(String title) {
+        return new Movie(title, Movie.REGULAR);
     }
 }
