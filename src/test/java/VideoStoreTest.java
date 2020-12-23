d3fec1e22391a8e62f91152b75f7b1c2a8701004
Juan <earth001@gmail.com>
2020-12-23 18:41:07 -0300

    Added additional refactoring

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -3,14 +3,19 @@ import org.junit.Test;
 
 public class VideoStoreTest {
 
-    private final Customer customer = new Customer("Fred");
+    private final Customer customer = new Customer("Fred",
+        new PricePlan(new LoyaltyPlan(), new Tariff()));
 
     @Test
     public void testSingleNewReleaseStatement() {
         customerRentsMovieForPeriod(newRelease("The Cell"), 3);
         Assert.assertEquals(
             "Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n",
-            customer.generateStatement());
+            generate());
+    }
+
+    private String generate() {
+        return customer.generateStatement(new StatementGenerator());
     }
 
     private void customerRentsMovieForPeriod(Movie movie, int daysRented) {
@@ -23,7 +28,7 @@ public class VideoStoreTest {
         customerRentsMovieForPeriod(newRelease("The Tigger Movie"), 3);
         Assert.assertEquals(
             "Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n",
-            customer.generateStatement());
+            generate());
     }
 
     @Test
@@ -31,7 +36,7 @@ public class VideoStoreTest {
         customerRentsMovieForPeriod(newChildrens("The Tigger Movie"), 3);
         Assert.assertEquals(
             "Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n",
-            customer.generateStatement());
+            generate());
     }
 
     @Test
@@ -42,7 +47,7 @@ public class VideoStoreTest {
 
         Assert.assertEquals(
             "Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n",
-            customer.generateStatement());
+            generate());
     }
 
     private Movie newRegular(String title) {
