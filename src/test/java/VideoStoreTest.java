2e01b536dbc531e517441a6b1c0abdfb4bb895be
Juan <earth001@gmail.com>
2020-12-29 17:09:37 -0300

    Added test

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,3 +1,9 @@
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matcher;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -5,6 +11,26 @@ public class VideoStoreTest {
 
     private final Customer customer = new Customer("Fred",
         new PricePlan(new LoyaltyPlan(), new Tariff()));
+
+    @Test
+    public void basic_tariff() {
+        Customer basic = new Customer("basic", new PricePlan(new LoyaltyPlan(), new Tariff()));
+        basic.addRental(new Rental(newChildrens("childrens"), 3));
+        basic.addRental(new Rental(newRegular("regular"), 3));
+        basic.addRental(new Rental(newRelease("new"), 3));
+
+        assertThat(basic.generateStatement(new StatementGenerator()),
+            hasStatementElement("Rental Record for basic",
+                "\tchildrens\t1.5",
+                "\tregular\t3.5",
+                "\tnew\t9.0",
+                "You owed 14.0",
+                "You earned 4 frequent renter points"));
+    }
+
+    private Matcher<String> hasStatementElement(String... lines) {
+        return equalTo(stream(lines).collect(joining("\n", "", "\n")));
+    }
 
     @Test
     public void testSingleNewReleaseStatement() {
